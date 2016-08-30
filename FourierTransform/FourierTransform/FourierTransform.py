62f879eec57bc1fb3600ccd35b5568bcb34adde8
Tiago Engel <Tiago Engel>
2016-08-29 23:56:19 -0300

    graphing audio, applying dft/idft

--- FourierTransform/FourierTransform/FourierTransform.py
+++ FourierTransform/FourierTransform/FourierTransform.py
@@ -1,9 +1,12 @@
 import numpy as np
 import wave
 import os
+from pylab import *
+import matplotlib.pyplot as plt
 
 def DFT_slow(x):
-    """Compute the discrete Fourier Transform of the 1D array x"""
+    """Compute the discrete Fourier Transform of the 1D array x
+    from https://jakevdp.github.io/blog/2013/08/28/understanding-the-fft/"""
     x = np.asarray(x, dtype=float)
     N = x.shape[0]
     n = np.arange(N)
@@ -11,14 +14,41 @@ def DFT_slow(x):
     M = np.exp(-2j * np.pi * k * n / N)
     return np.dot(M, x)
 
+def IDFT_slow(x):
+    """Compute the inverse discrete Fourier Transform of the 1D array x"""
+    x = np.asarray(x, dtype=complex)
+    N = x.shape[0]
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    M = np.exp(2j * np.pi * k * n / N)
+    return 1.0/N * np.dot(M, x)
+
 #load audio
-w = wave.open("./audio-samples/dogs-n-birds.wav", "rb")
+w = wave.open("./audio-samples/lz-s2h-beginning.wav", "rb")
 print('Frames: ' + str(w.getnframes()))
 print('Channels: ' + str(w.getnchannels()))
-frames = w.readframes(8)
+frames = w.readframes(256)
 array = np.frombuffer(frames, dtype = "ubyte")
 
-#for x in array:
-#    print(str(x))
+#test dft
+fff = DFT_slow(array)
+iff = IDFT_slow(fff)
+print(np.allclose(fff, np.fft.fft(array))) # verify our dft against known fft implementation
+print(np.allclose(IDFT_slow(fff), np.fft.ifft(fff))) # verify our idft against known ifft implementation
 
-print(DFT_slow(array))
+#get the real part
+iff_real = []
+for i in range(0, iff.shape[0], 1):
+    iff_real.append(iff[i].real * 0.9) # Scale a little bit to look different
+
+#gen graph
+N = len(iff_real)
+t = arange(0, N)
+plot(t, iff_real)
+plot(t, array)
+axis([0, N, amin(array)*10, amax(array)]) #[minx, maxx, miny,maxy]
+xlabel('sample')
+ylabel('amplitude')
+title('Stairway To Heaven DFT')
+
+show()
