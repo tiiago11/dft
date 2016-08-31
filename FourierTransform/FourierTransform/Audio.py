6b6b6a52bfdd2fde4a2c6c4730e62500eb660cee
Alex Frasson <afrasson@inf.ufsm.br>
2016-08-31 13:45:21 -0300

    New Audio and Image code files.

---
+++ FourierTransform/FourierTransform/Audio.py
@@ -0,0 +1,41 @@
+######################################################
+########################## test 1D DFT with audio wave
+######################################################
+
+import wave
+import numpy as np
+from pylab import *
+import FourierTransform as ourft
+
+#load audio
+#w = wave.open("./audio-samples/lz-s2h-beginning.wav", "rb")
+w = wave.open("./audio-samples/bass.wav", "rb")
+print('Frames: ' + str(w.getnframes()))
+print('Channels: ' + str(w.getnchannels()))
+frames = w.readframes(512)
+array = np.frombuffer(frames, dtype = "ubyte")
+
+#test dft
+fff = ourft.DFT_slow(array)
+iff = ourft.IDFT_slow(fff)
+print(np.allclose(fff, np.fft.fft(array))) # verify our dft against known fft implementation
+print(np.allclose(ourft.IDFT_slow(fff), np.fft.ifft(fff))) # verify our idft against known ifft implementation
+
+#truncation
+print("input samples: ", len(array))
+#ff_truncated = ourft.zero_upper_range(fff, 200) # set to zero values larger than
+ff_truncated = ourft.zero_lower_range(fff, 10)   # set to zero values lower than
+iff_truncated = ourft.IDFT_slow(ff_truncated) # reconstruct the signal
+
+#gen graph
+N = len(iff)
+t = np.arange(0, N)
+plot(t, iff_truncated)
+plot(t, array)
+axis([0, N, amin(array), amax(array)]) #[minx, maxx, miny,maxy]
+xlabel('sample')
+ylabel('amplitude')
+title('Stairway To Heaven DFT')
+
+# plot audio wave
+show()
--- FourierTransform/FourierTransform/FourierTransform.py
+++ FourierTransform/FourierTransform/FourierTransform.py
@@ -1,9 +1,4 @@
 import numpy as np
-import wave
-import os
-from pylab import *
-import matplotlib.pyplot as pl
-from PIL import Image
 
 def DFT_slow(x):
     """Compute the discrete Fourier Transform of the 1D array x
@@ -88,88 +83,4 @@ def zero_lower_range(x, lower_threshold):
             count+=1
     print("zeroed samples: ", count)
     return x;
-
-######################################################
-########################## test 1D DFT with audio wave
-######################################################
-
-##load audio
-#w = wave.open("./audio-samples/lz-s2h-beginning.wav", "rb")
-#print('Frames: ' + str(w.getnframes()))
-#print('Channels: ' + str(w.getnchannels()))
-#frames = w.readframes(512)
-#array = np.frombuffer(frames, dtype = "ubyte")
-
-##test dft
-#fff = DFT_slow(array)
-#iff = IDFT_slow(fff)
-#print(np.allclose(fff, np.fft.fft(array))) # verify our dft against known fft implementation
-#print(np.allclose(IDFT_slow(fff), np.fft.ifft(fff))) # verify our idft against known ifft implementation
-
-##truncation
-#print("input samples: ", len(array))
-##ff_truncated = zero_upper_range(fff, 200) # set to zero values larger than
-#ff_truncated = zero_lower_range(fff, 10)   # set to zero values lower than
-#iff_truncated = IDFT_slow(ff_truncated) # reconstruct the signal
-
-##gen graph
-#N = len(iff)
-#t = arange(0, N)
-#plot(t, iff_truncated)
-#plot(t, array)
-#axis([0, N, amin(array), amax(array)]) #[minx, maxx, miny,maxy]
-#xlabel('sample')
-#ylabel('amplitude')
-#title('Stairway To Heaven DFT')
-
-#show() # plot audio wave
-
-###################################################
-########################## Test 2D DFT with image
-###################################################
-
-#load image
-img = Image.open("images/pulse_512_diag.png").convert("L") # use greyscale
-pixels = np.asarray(img.getdata(), dtype=np.float32).reshape((img.size[1],img.size[0]))
-(M,N) = img.size
-
-#adjust frequencies so the center of the spectrum is at the center of the image.
-for x in range(M):
-    for y in range(N):        
-        pixels[x,y] *= np.power(-1, x+y) # according to gonzales, the -1^(x+y) is to center the spectrum
-
-##########
-#compute forward 
-print("Calculating direct...")
-ff2d = DFT2D_slow(pixels)
-#print(np.allclose(ff2d, np.fft.fft2(pixels)))
-
-##########
-#compute inverse 
-print("Calculating inverse...")
-iff2d = IDFT2D_slow(ff2d)
-#print(np.allclose(iff2d, np.fft.ifft2(ff2d)))
-
-##########
-#compare original signal to the inverse
-#print(np.allclose(iff2d, pixels))
-
-###########
-#### save reconstructed image
-#image = Image.new("L", (M, N))
-#img_data = image.load()
-#for x in range(M):
-#    for y in range(N):
-#        img_data[x,y] = int(iff2d[y,x].real)
-#image.save("output.png", "PNG")
-
-##########
-## Save magnitude spectrum image
-magMap = calculate_magnitude_2d(ff2d)
-image = Image.new("L", (M, N))
-img_data = image.load() 
-for x in range(M):
-    for y in range(N):
-        img_data[x,y] = 15 * int(np.log(magMap[y,x])) #magnitude highlight: 
-image.save("output_mag.png", "PNG")
 
