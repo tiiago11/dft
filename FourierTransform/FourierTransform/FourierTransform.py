a44bb825532bb1f066826f271277bc91a74c9094
tiiago11 <tengel@inf.ufsm.br>
2016-08-31 10:41:19 -0300

    fixed spectrum map, added log scale

--- FourierTransform/FourierTransform/FourierTransform.py
+++ FourierTransform/FourierTransform/FourierTransform.py
@@ -129,35 +129,47 @@ def zero_lower_range(x, lower_threshold):
 ###################################################
 
 #load image
-img = Image.open("images/lena_128.png").convert("L") # use greyscale
-pixels = np.asarray(img.getdata(),dtype=np.float32).reshape((img.size[1],img.size[0]))
+img = Image.open("images/pulse_512_diag.png").convert("L") # use greyscale
+pixels = np.asarray(img.getdata(), dtype=np.float32).reshape((img.size[1],img.size[0]))
 (M,N) = img.size
 
+#adjust frequencies so the center of the spectrum is at the center of the image.
+for x in range(M):
+    for y in range(N):        
+        pixels[x,y] *= np.power(-1, x+y) # according to gonzales, the -1^(x+y) is to center the spectrum
+
+##########
 #compute forward 
+print("Calculating direct...")
 ff2d = DFT2D_slow(pixels)
-print(np.allclose(ff2d, np.fft.fft2(pixels)))
+#print(np.allclose(ff2d, np.fft.fft2(pixels)))
+
+##########
 #compute inverse 
+print("Calculating inverse...")
 iff2d = IDFT2D_slow(ff2d)
-print(np.allclose(iff2d, np.fft.ifft2(ff2d)))
+#print(np.allclose(iff2d, np.fft.ifft2(ff2d)))
+
+##########
 #compare original signal to the inverse
-print(np.allclose(iff2d, pixels))
+#print(np.allclose(iff2d, pixels))
 
+###########
 #### save reconstructed image
-image = Image.new("L", (M, N))
-img_data = image.load()
-for x in range(M):
-    for y in range(N):
-        img_data[y,x] = int(iff2d[x,y].real)
+#image = Image.new("L", (M, N))
+#img_data = image.load()
+#for x in range(M):
+#    for y in range(N):
+#        img_data[x,y] = int(iff2d[y,x].real)
+#image.save("output.png", "PNG")
 
-image.save("output.png", "PNG")
-
-## Save magnitude image
-# not sure if this is right
+##########
+## Save magnitude spectrum image
 magMap = calculate_magnitude_2d(ff2d)
-image2 = Image.new("L", (M, N))
-img_data2 = image2.load() 
+image = Image.new("L", (M, N))
+img_data = image.load() 
 for x in range(M):
     for y in range(N):
-        img_data2[y,x] = int(magMap[x,y])
-image2.save("output_mag.png", "PNG")
+        img_data[x,y] = 15 * int(np.log(magMap[y,x])) #magnitude highlight: 
+image.save("output_mag.png", "PNG")
 
