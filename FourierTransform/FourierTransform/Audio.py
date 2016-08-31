9aed5bd92dcdde35e4d6d31d78989a3d46021bd6
Alex Frasson <afrasson@inf.ufsm.br>
2016-08-31 14:58:29 -0300

    Added legend to audio plot and trunc threshold to title.

--- FourierTransform/FourierTransform/Audio.py
+++ FourierTransform/FourierTransform/Audio.py
@@ -1,18 +1,17 @@
 ######################################################
 ########################## test 1D DFT with audio wave
 ######################################################
-
 import wave
 import numpy as np
 from pylab import *
 import FourierTransform as ourft
 
 #load audio
-#w = wave.open("./audio-samples/lz-s2h-beginning.wav", "rb")
-w = wave.open("./audio-samples/bass.wav", "rb")
+w = wave.open("./audio-samples/lz-s2h-solo.wav", "rb")
+#w = wave.open("./audio-samples/bass.wav", "rb")
 print('Frames: ' + str(w.getnframes()))
 print('Channels: ' + str(w.getnchannels()))
-frames = w.readframes(512)
+frames = w.readframes(64)
 array = np.frombuffer(frames, dtype = "ubyte")
 
 #test dft
@@ -24,18 +23,20 @@ print(np.allclose(ourft.IDFT_slow(fff), np.fft.ifft(fff))) # verify our idft aga
 #truncation
 print("input samples: ", len(array))
 #ff_truncated = ourft.zero_upper_range(fff, 200) # set to zero values larger than
-ff_truncated = ourft.zero_lower_range(fff, 10)   # set to zero values lower than
+truncatebelow = 10
+ff_truncated = ourft.zero_lower_range(fff, truncatebelow)   # set to zero values lower than
 iff_truncated = ourft.IDFT_slow(ff_truncated) # reconstruct the signal
 
 #gen graph
 N = len(iff)
 t = np.arange(0, N)
-plot(t, iff_truncated)
-plot(t, array)
+plot(t, iff_truncated, label='reconstructed')
+plot(t, array, label='original')
+legend(loc='upper right')
 axis([0, N, amin(array), amax(array)]) #[minx, maxx, miny,maxy]
 xlabel('sample')
 ylabel('amplitude')
-title('Stairway To Heaven DFT')
+title('Truncated DFT below ' + str(truncatebelow))
 
 # plot audio wave
 show()
